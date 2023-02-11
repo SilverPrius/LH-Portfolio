f094e6f2e7e0e2fca6a2d73bc4113ff9ab2e4e0c
Long <hoangcodes@gmail.com>
2023-02-10 19:26:51 -0600

    Hamburger nav bar is working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,39 @@
 import '../components/nav.css'
 
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { FaBars } from '/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function Nav() {
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
     return (
-        <div className="nav">
-            <Link to="/">
-                <div>Home</div>
-            </Link>
-            <Link to="/about">
-                <div>About</div>
-            </Link>
-            <Link to="/projects">
-                <div>Projects</div>
-            </Link>
-            <Link to="/contact">
-                <div>Contact</div>
-            </Link>
-            <div className='hamburger'>
-                <Fabars />
+        <div className="header">
+            <Link to="/"><h1 className='logo'>LH</h1></Link>
+            <ul className={click ? "nav-menu active" :
+                "nav-menu"}>
+                <li>
+                    <Link to="/"><div>Home</div></Link>
+                </li>
+                <li>
+                    <Link to="/about"><div>About</div></Link>
+                </li>
+                <li>
+                    <Link to="/projects"><div>Projects</div></Link>
+                </li>
+                <li>
+                    <Link to="/contact"><div>Contact</div></Link>
+                </li>
+            </ul>
+            <div className='hamburger' onClick=
+                {handleClick}>
+                {click ? (<FaTimes size={20} style={{ color: "#fff" }
+                } />) : (<FaBars size={20} style={{ color: "#fff" }
+                } />)}
+
+
             </div>
         </div>
     )
