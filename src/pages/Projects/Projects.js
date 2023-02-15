5bbf89b9ef85025676091fb59f928d6991ec3624
Long <hoangcodes@gmail.com>
2023-02-14 19:53:40 -0600

    Updated wireframe and CSS styling for Projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -12,27 +12,28 @@ const Projects = () => {
       <div className='projects-inner'>
         <div className='projects-title'>MY PROJECTS<hr></hr></div>
         <div className='projects'>
-            {projects.map((project, index) => (
-                <div key={index} className='project-container'>
-                    <h2 className="project-title">{project.title}</h2><br></br>
-                    <hr></hr>
-                    <br></br><h3 className="project-subtitle">{project.subtitle}</h3>
-                    <br></br><p className="project-description">{project.description}</p>
-                    <br></br><img src={project.image} alt={project.title} onError={(e) => console.log(e.target.error)} className='project-image' />
-                    <div className="logo">
-                        <a href={project.link} >
-                            <FontAwesomeIcon className="external-link-logo" icon={faExternalLinkAlt} title="Live Demo" />
-                        </a>
-                        <a href={project.github} >
-                            <FontAwesomeIcon className="github-logo" icon={faGithub} title="Repository" />
-                        </a>
-                    </div>
+          {projects.map((project, index) => (
+            <div key={index} className={`project-container ${index === 1 ? 'reverse' : ''}`}>
+              <img src={project.image} alt={project.title} onError={(e) => console.log(e.target.error)} className='project-image' />
+              <div className="project-info">
+                <h2 className="project-title">{project.title}</h2>
+                <br /><h3 className="project-subtitle">{project.subtitle}</h3>
+                <h3>LIVE DEMO</h3><div className="logo">
+                  <a href={project.link} >
+                    <FontAwesomeIcon className="external-link-logo" icon={faExternalLinkAlt} title="Live Demo" />
+                  </a>
+                  <a href={project.github} >
+                    <FontAwesomeIcon className="github-logo" icon={faGithub} title="Repository" />
+                  </a>
                 </div>
-            ))}
+                <p className="project-description">{project.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
 
-export default Projects
+export default Projects 
