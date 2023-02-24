5657cd94134f8c76553d0c792636b8c5d5a7e5d6
Long <long88hoang@gmail.com>
2023-02-24 17:42:48 -0600

    Added rotating words to home page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-require('dotenv').config()
+// require('dotenv').config()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,15 @@ const Home = () => {
       <div className="my-name">
         <h1>LONG HOANG</h1>
       </div>
-      <div className='my-roles'>Software Engineer</div>
+      <div class="wrapper">
+       <div class="rotating-words">
+           <span>Software Engineer</span>
+           <span>MERN Stack</span>
+           <span>Frontend Developer</span>
+           <span>Fullstack Developer</span>
+           <span>Software Engineer</span>
+       </div>
+   </div>
     </div>
   )
 }
