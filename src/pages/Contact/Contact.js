03c4da42021b6d4320bbc58920d5c9545ad405d7
Long <long88hoang@gmail.com>
2023-02-26 19:02:56 -0600

    Removed custom success message for contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -134,26 +134,6 @@
 import '../Contact/contact.css'
 
 const Contact = () => {
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => alert("Thank you for your message"))
-      .catch((error) => alert(error));
-  };
-  
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-
   return (
     <div className='contact-main'>
       <div className='contact-inner'>
