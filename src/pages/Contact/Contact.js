d9241e6450e449ad544de07fb76bbe0d7310ad44
Long <long88hoang@gmail.com>
2023-02-27 19:04:05 -0600

    Adding contact info to contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -137,40 +137,55 @@ const Contact = () => {
   return (
     <div className='contact-main'>
       <div className='contact-inner'>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div hidden>
-            <input name="bot-field" />
+        <div className='contact-title'>CONTACT<hr></hr></div>
+        <div className='contact-body'>
+          <div className='contact-info'>
+            <p className='contact-name'><strong>Long Hoang</strong></p>
+            <br />
+            <p className='contact-email'><a href={"mailto:hoangcodes@gmail.com"}><strong>Email: </strong> hoangcodes@gmail.com</a></p> 
+            
+            
+            <br />
+            <p className='contact-phone'><strong>Phone: </strong>(510) 219-7788</p>
+            <br />
+            <p className='contact-message'>I look forward to collaborating with you!</p>
           </div>
-          <div className='contact-title'>Let's Connect 💡</div>
-          <div className='form-inputs'>
-          <div>
-            <label>First name<br />
-              <input type="text" name="first-name" required />
-            </label>
-          </div>
-          <div>
-            <label>Last name<br />
-              <input type="text" name="last-name" required  />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="email">Email</label><br />
-            <input id="email" type="email" name="email" required  />
-          </div>
-          <div>
-            <label>Message<br />
-              <textarea name="message"  required cols={30} rows={10} ></textarea>
-            </label>
-          </div>
-          </div>
-          <button className='btn' type="submit">Send</button>
-        </form>
+
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+              <input name="bot-field" />
+            </div>
+
+            <div className='form-inputs'>
+              <div>
+                <label>First name<br />
+                  <input type="text" name="first-name" required />
+                </label>
+              </div>
+              <div>
+                <label>Last name<br />
+                  <input type="text" name="last-name" required />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="email">Email</label><br />
+                <input id="email" type="email" name="email" required />
+              </div>
+              <div>
+                <label>Message<br />
+                  <textarea name="message" required cols={30} rows={10} ></textarea>
+                </label>
+              </div>
+            </div>
+            <button className='btn' id='contact-button' type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   )
