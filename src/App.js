a79e541ca6752f8e01a8bacbfcdf3cc477803770
Long <long88hoang@gmail.com>
2023-03-01 21:03:25 -0600

    Removed footer from contact page and adjusted height on pages to fit footer on screen without having to scroll down

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { Route, Routes} from "react-router-dom"
-
-// Components
+import { Route, Routes, useLocation } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-
-// Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
-
-
-// CSS
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const location = useLocation();
+
+  // Check if the current route is the Contact page
+  const isContactPage = location.pathname === "/contact";
+
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/projects" component={Projects} element={<Projects/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
+      {!isContactPage && <Footer />}
     </div>
   );
 }
