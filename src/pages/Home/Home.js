d55def4a4bc89b7d4111bf85d9e78646ec739a70
Long <long88hoang@gmail.com>
2023-03-06 16:03:19 -0600

    Updated homepage text and animations

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,30 @@
 import '../Home/home.css';
+import { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 
 
 const Home = () => {
+
+  useEffect(() => {
+    ScrollReveal().reveal('.my-name', { origin: 'top', distance: '30rem', duration: 2000});
+    ScrollReveal().reveal('.your', { delay: 2000, origin: 'left', distance: '1rem', duration: 2000});
+    ScrollReveal().reveal('.wrapper', { delay: 4000, scale: 0.5, duration: 2000});
+  }, []);
+
   return (
     <div className="home-main">
       <div className="my-name">
-        <h1>LONG HOANG</h1>
+        <p className='hi'>Hi, I'm <strong>Long Hoang</strong>.</p>
+        <p className='your'>Your friendly, neighborhood</p>
+        {/* <h1>LONG HOANG</h1> */}
       </div>
       <div class="wrapper">
         <div class="rotating-words">
-          <span>Software Engineer</span>
-          <span>MERN Stack</span>
-          <span>Frontend Developer</span>
-          <span>Fullstack Developer</span>
-          <span>Software Engineer</span>
+          <span>Software Engineer.</span>
+          <span>Web Developer.</span>
+          <span>Frontend Developer.</span>
+          <span>Fullstack Developer.</span>
+          <span>Software Engineer.</span>
         </div>
       </div>
     </div>
