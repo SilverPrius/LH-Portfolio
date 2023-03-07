2968d950e198761c1e3997863effb23641bc46d3
Long <long88hoang@gmail.com>
2023-03-06 18:32:25 -0600

    Added spiderman image to Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,18 +6,20 @@ import ScrollReveal from 'scrollreveal';
 const Home = () => {
 
   useEffect(() => {
-    ScrollReveal().reveal('.my-name', { origin: 'top', distance: '30rem', duration: 2000});
-    ScrollReveal().reveal('.your', { delay: 2000, origin: 'left', distance: '1rem', duration: 2000});
-    ScrollReveal().reveal('.wrapper', { delay: 4000, scale: 0.5, duration: 2000});
+    ScrollReveal().reveal('.my-name', { origin: 'top', distance: '30rem', duration: 2000 });
+    ScrollReveal().reveal('.your', { delay: 2000, origin: 'left', distance: '1rem', duration: 2000 });
+    ScrollReveal().reveal('.wrapper', { delay: 4000, scale: 0.75, duration: 2000 });
+    ScrollReveal().reveal('.home-message', { delay: 8000, scale: 0.6, duration: 2000 });
+    ScrollReveal().reveal('.spiderman', { delay: 10000, origin: 'top', distance: '15rem', duration: 2000 });
   }, []);
 
   return (
     <div className="home-main">
       <div className="my-name">
         <p className='hi'>Hi, I'm <strong>Long Hoang</strong>.</p>
+        </div>
         <p className='your'>Your friendly, neighborhood</p>
-        {/* <h1>LONG HOANG</h1> */}
-      </div>
+      
       <div class="wrapper">
         <div class="rotating-words">
           <span>Software Engineer.</span>
@@ -26,6 +28,10 @@ const Home = () => {
           <span>Fullstack Developer.</span>
           <span>Software Engineer.</span>
         </div>
+      </div>
+      <div className='save'>
+        <p className='home-message'>Let's save the web together.</p>
+        <img src="https://i.postimg.cc/5tmzT0tv/15b98f4047091fe00d5c44438e9f9dc6-removebg-preview.png" alt="img-spiderman" class="spiderman" />
       </div>
     </div>
   );
