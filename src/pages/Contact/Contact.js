a436482c7f35bf7a067954c6ef8eb28e526061d6
Long <long88hoang@gmail.com>
2023-03-31 15:16:32 -0500

    Removed italics from contact-card

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,9 +7,9 @@ import ScrollReveal from 'scrollreveal';
 const Contact = () => {
 
   useEffect(() => {
-    ScrollReveal().reveal('.contact-title', { delay: 500, origin: 'top', distance: '10rem', duration: 2000});
-    ScrollReveal().reveal('address', { delay: 800, origin: 'left', distance: '10rem', duration: 1000});
-    ScrollReveal().reveal('.form', { delay: 800, origin: 'right', distance: '10rem',duration: 1000});
+    ScrollReveal().reveal('.contact-title', { delay: 500, origin: 'top', distance: '10rem', duration: 2000 });
+    ScrollReveal().reveal('.contact-card', { delay: 800, origin: 'left', distance: '10rem', duration: 1000 });
+    ScrollReveal().reveal('.form', { delay: 800, origin: 'right', distance: '10rem', duration: 1000 });
   }, []);
 
   return (
@@ -17,62 +17,62 @@ const Contact = () => {
       <div className='contact-inner'>
         <div className='contact-title'>Contact Me<hr></hr></div>
         <div className='contact-body'>
-            <address>
-              <p className='contact-info-message'>Let's Stay Connected</p>
-              <br />
-              
-              <p className='contact-name'><strong>Long Hoang</strong></p>
-              <br />
-              <div className='contact-info'>
+          <div className='contact-card'>
+            <p className='contact-info-message'>Let's Stay Connected</p>
+            <br />
+
+            <p className='contact-name'><strong>Long Hoang</strong></p>
+            <br />
+            <div className='contact-info'>
               <p><FontAwesomeIcon className="icon" icon={faMobileScreen} /><strong> Phone: </strong>(510) 219 - 7788</p>
               <br />
               <p><FontAwesomeIcon className="icon" icon={faEnvelope} /><strong> Email: </strong> <a className='email-link' href={"mailto:hoangcodes@gmail.com"}>Hoangcodes@gmail.com</a></p>
               <br />
               <div className='resume'><p><FontAwesomeIcon className="icon" icon={faDownload} /><strong> Resume: </strong></p><a className='btn' id='download-btn' href="../assets/Long_Hoang_Resume.pdf" download>Download</a></div>
-              <br/>
-              </div>
-              <p className='contact-message'><strong>I look forward to collaborating with you!</strong></p>
-        </address>
-        <div className='form'>
-          <div className='form-message'>Leave Me A Message</div>
-          <br />
-          <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="form-name" value="contact" />
-            <div hidden>
-              <input name="bot-field" />
+              <br />
             </div>
+            <p className='contact-message'><strong>I look forward to collaborating with you!</strong></p>
+          </div>
+          <div className='form'>
+            <div className='form-message'>Leave Me A Message</div>
+            <br />
+            <form
+              name="contact"
+              method="POST"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <div hidden>
+                <input name="bot-field" />
+              </div>
 
-            <div className='form-inputs'>
-              <div>
-                <label>First name<br />
-                  <input type="text" name="first-name" required />
-                </label>
+              <div className='form-inputs'>
+                <div>
+                  <label>First name<br />
+                    <input type="text" name="first-name" required />
+                  </label>
+                </div>
+                <div>
+                  <label>Last name<br />
+                    <input type="text" name="last-name" required />
+                  </label>
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label><br />
+                  <input id="email" type="email" name="email" required />
+                </div>
+                <div>
+                  <label>Message<br />
+                    <textarea name="message" required cols={30} rows={10} ></textarea>
+                  </label>
+                </div>
               </div>
-              <div>
-                <label>Last name<br />
-                  <input type="text" name="last-name" required />
-                </label>
-              </div>
-              <div>
-                <label htmlFor="email">Email</label><br />
-                <input id="email" type="email" name="email" required />
-              </div>
-              <div>
-                <label>Message<br />
-                  <textarea name="message" required cols={30} rows={10} ></textarea>
-                </label>
-              </div>
-            </div>
-            <button className='btn' id='contact-button' type="submit">Send</button>
-          </form>
+              <button className='btn' id='contact-button' type="submit">Send</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div >
   )
 }
